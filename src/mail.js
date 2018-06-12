e0bd494c40cafe3adfeb0dd82ab86f7b49d2561b
Will King <contact@wking.io>
2018-06-12 18:03:55 -0500

    Adding recover and reset API calls for the reset flow

---
+++ src/mail.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const email = {
+  async send(options) {
+    const mailOptions = {
+      from: 'William King <contact@wking.io>',
+      to: options.user.email,
+      subject: options.subject,
+      html: `Hey, you can reset you password here: <a href="${
+        options.resetUrl
+      }">Reset Password</a>`,
+      text: `Hey, you can reset you password here: ${options.resetUrl}`,
+    };
+
+    return transport.sendMail(mailOptions);
+  },
+};
+
+module.exports = email;
--- src/resolvers/Mutation/auth.js
+++ src/resolvers/Mutation/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const email = require('../../email');
+const crypto = require('crypto');
+const mail = require('../../mail');
 
 const auth = {
   async signup(parent, args, ctx, info) {
@@ -32,11 +33,53 @@ const auth = {
     };
   },
 
-  // async recover(parent, { email }, ctx, info) {
-  //   const user = await ctx.db.query.user({ where: { email } });
-  //   if (user) {
-  //   }
-  // },
+  async recover(parent, { email, baseUrl }, ctx, info) {
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email: ${email}`);
+    }
+    const resetToken = crypto.randomBytes(20).toString('hex') + Date.now();
+    const resetExpires = new Date(Date.now() + 360000);
+    const resetUrl = `${baseUrl}/reset/${resetToken}`;
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email },
+      data: { resetToken, resetExpires },
+    });
+
+    await mail.send({
+      user: updatedUser,
+      subject: 'Password Reset - WatchWord',
+      resetUrl,
+    });
+
+    return updatedUser;
+  },
+
+  async reset(parent, { resetToken, ...args }, ctx, info) {
+    const user = await ctx.db.query.user({
+      where: { resetToken },
+    });
+    if (!user) {
+      throw new Error(`Your reset token was not found.`);
+    }
+
+    if (user.resetExpires === null || user.resetExpires > Date.now()) {
+      throw new Error(`Your reset token is expired.`);
+    }
+
+    const match = await bcrypt.compare(args.password, user.password);
+    if (match) {
+      throw new Error(`You cannot reset your password to the same password.`);
+    }
+
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { resetToken },
+      data: { password, resetExpires: null },
+    });
+
+    return updatedUser;
+  },
 };
 
 module.exports = { auth };
