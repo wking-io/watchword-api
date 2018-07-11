61264b01a0297eb6bc87a57d69f2cd4f4f2a8390
Will King <contact@wking.io>
2018-07-11 15:45:56 -0500

    Removing apollo-engine until I understand it better

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const { formatError } = require('apollo-errors');
-const { ApolloEngine } = require('apollo-engine');
 const createServer = require('./createServer');
 const db = require('./db');
 const { getToken, verifyToken, safeProp, findUser } = require('./utils');
@@ -24,6 +23,9 @@ server.express.use(async (req, res, next) => {
 
 const options = {
   port: parseInt(process.env.PORT, 10) || 8000,
+  endpoint: '/graphql',
+  subscriptions: '/subscriptions',
+  playground: '/playground',
   formatError,
   cors: {
     credentials: true,
@@ -31,33 +33,8 @@ const options = {
   },
 };
 
-if (process.env.APOLLO_ENGINE_KEY) {
-  const engine = new ApolloEngine({
-    apiKey: process.env.APOLLO_ENGINE_KEY,
-  });
-
-  const httpServer = server.createHttpServer({
-    tracing: true,
-    cacheControl: true,
-  });
-
-  engine.listen(
-    {
-      httpServer,
-      ...options,
-    },
-    () =>
-      console.log(
-        `Server with Apollo Engine is running on http://localhost:${
-          options.port
-        }`
-      )
-  );
-} else {
-  graphQLServer.start(
-    {
-      port,
-    },
-    () => console.log(`Server is running on http://localhost:${options.port}`)
-  );
-}
+server.start(options, ({ port }) =>
+  console.log(
+    `Server started, listening on port ${port} for incoming requests.`
+  )
+);
